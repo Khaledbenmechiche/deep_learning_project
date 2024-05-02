50336215dc7422d833f1d88579b6d95ce7f77cc2
Khaledbenmechiche <116712640+Khaledbenmechiche@users.noreply.github.com>
2024-05-02 12:57:59 +0200

    last version

--- data_viz.py
+++ data_viz.py
@@ -6,6 +6,7 @@ from folium.plugins import MarkerCluster
 
 # Chargement du fichier de données traitées
 df_viz = pd.read_csv("data/donnees_traitees.csv")
+df_pred = pd.read_csv("data/predictions.csv")
 
 # Titre de l'application
 st.title("Analyse des points de charge pour véhicules électriques")
@@ -42,7 +43,8 @@ columns_to_analyze = [
     'date_mise_en_service',
     'consolidated_is_lon_lat_correct',
     'consolidated_is_code_insee_verified',
-    'nom_departement'
+    'nom_departement',
+    'puissance_nominale'
 ]
 
 # Sélection de la colonne à analyser à l'aide d'un widget de sélection (filtre)
@@ -60,13 +62,10 @@ if selected_column:
     st.bar_chart(value_counts)
 
 
-
-
-
 # Filtrer les données pour les colonnes pertinentes
 df_filtered = df_viz[['id_pdc_itinerance', 'consolidated_longitude', 'consolidated_latitude', 'nom_departement', 'nb_vp_rechargeables_el', 'nom_region']]
 
-# Titre de l'application
+# Titre de la première carte
 st.title("Répartition des points de charge et des véhicules électriques sur la carte")
 
 # Créer une carte centrée sur la France
@@ -87,21 +86,52 @@ for index, row in df_filtered.iterrows():
     nb_vp = row['nb_vp_rechargeables_el']
     region = row['nom_region']
     
-    # Créer un popup pour le point de charge
-    popup_text_pdc = f"ID: {id_pdc}<br>Latitude: {latitude}<br>Longitude: {longitude}<br>Département: {departement}"
-    
-    # Ajouter un marqueur pour chaque point de charge au cluster
-    folium.Marker([latitude, longitude], popup=popup_text_pdc).add_to(marker_cluster_pdc)
-    
-    # Créer un cluster de marqueurs pour les véhicules par région si la région n'existe pas encore
-    if region not in region_clusters:
-        region_clusters[region] = MarkerCluster().add_to(m)
-    
-    # Créer un popup pour le nombre de véhicules électriques par région
-    popup_text_vp = f"Région: {region}<br>Nombre de véhicules électriques: {nb_vp}"
-    
-    # Ajouter un marqueur pour le nombre de véhicules électriques à son cluster de région correspondant
-    folium.Marker([latitude, longitude], popup=popup_text_vp).add_to(region_clusters[region])
+    # Vérifier si les coordonnées sont valides
+    if not pd.isnull(latitude) and not pd.isnull(longitude):
+        # Créer un popup pour le point de charge
+        popup_text_pdc = f"ID: {id_pdc}<br>Latitude: {latitude}<br>Longitude: {longitude}<br>Département: {departement}"
+        
+        # Ajouter un marqueur pour chaque point de charge au cluster
+        folium.Marker([latitude, longitude], popup=popup_text_pdc).add_to(marker_cluster_pdc)
+        
+        # Créer un cluster de marqueurs pour les véhicules par région si la région n'existe pas encore
+        if region not in region_clusters:
+            region_clusters[region] = MarkerCluster().add_to(m)
+        
+        # Créer un popup pour le nombre de véhicules électriques par région
+        popup_text_vp = f"Région: {region}<br>Nombre de véhicules électriques: {nb_vp}"
+        
+        # Ajouter un marqueur pour le nombre de véhicules électriques à son cluster de région correspondant
+        folium.Marker([latitude, longitude], popup=popup_text_vp).add_to(region_clusters[region])
 
 # Afficher la carte Folium dans Streamlit
 folium_static(m)
+
+
+# Créer une nouvelle carte pour afficher les informations de prédiction
+st.title("Prédictions sur les points de charge")
+
+# Créer une nouvelle carte centrée sur la France
+m_pred = folium.Map(location=[46.603354, 1.888334], zoom_start=6)
+
+# Créer un cluster de marqueurs pour les prédictions
+marker_cluster_pred = MarkerCluster().add_to(m_pred)
+
+# Filtrer les données de prédiction pour exclure les lignes avec des NaN
+df_pred_filtered = df_pred.dropna(subset=['consolidated_latitude', 'consolidated_longitude'])
+
+# Parcourir chaque ligne du DataFrame filtré des prédictions
+for idx, row_pred in df_pred_filtered.iterrows():
+    id_pdc_pred = row_pred['id_pdc_itinerance']
+    latitude_pred = row_pred['consolidated_latitude']
+    longitude_pred = row_pred['consolidated_longitude']
+    prediction = row_pred['Prediction']
+    
+    # Créer un popup pour la prédiction
+    popup_text_pred = f"ID: {id_pdc_pred}<br>Latitude: {latitude_pred}<br>Longitude: {longitude_pred}<br>Prediction: {prediction}"
+    
+    # Ajouter un marqueur pour chaque prédiction au cluster
+    folium.Marker([latitude_pred, longitude_pred], popup=popup_text_pred).add_to(marker_cluster_pred)
+
+# Afficher la carte Folium avec les prédictions dans Streamlit
+folium_static(m_pred)
